72070451a32e8d0352866aeb7a84d196251dcfd8
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2025-05-29 10:54:55 +0800

    pushing migrations among other things

---
+++ database/migrations/2025_05_29_014904_create_comments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->text('comment_content')->comment('Content of comment');
+            $table->timestamp('comment_date')->comment('Date of comment');
+            $table->string('reviewer_name')->comment('name of reviewer');
+            $table->string('reviewer_email')->commment('email of reviewer');
+            $table->boolean('is_hidden')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
